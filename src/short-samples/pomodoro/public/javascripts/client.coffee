5c4da7dbfdd281ac589d4d17b9b55ee1a2230076
Daniel Cheah <me@dancheah.com>
2011-03-25 23:44:21 -0700

    Doing a little refactoring of the code

--- src/short-samples/pomodoro/public/javascripts/client.coffee
+++ src/short-samples/pomodoro/public/javascripts/client.coffee
@@ -1,7 +1,7 @@
 show_pomodoro_timer = (e) -> 
   console.log($(this).attr('id'))
   $('#todo_form').hide()
-  $('#pomodoro_timer').show()
+  $('#pomodoro').show()
 
 toggle_buttons = () ->
     a = $('button#pomodoro_interrupt').attr('disabled')
@@ -11,6 +11,18 @@ toggle_buttons = () ->
 
 sound_alarm = () ->
   $('#audiofile')[0].play()
+
+pomodoro_object =
+  # 25 Minutes
+  InitialRemainingTime: 1000 * 60 * 25 
+
+  # 15 Seconds
+  InitialInterruptTime: 1000 * 15
+
+  remainingTime: -1
+  interruptTime: -1
+
+  getInitialRemainingTime:
 
 format_countdown = (milliseconds) ->
   secondsRemaining = milliseconds % 60000 
@@ -24,37 +36,74 @@ format_countdown = (milliseconds) ->
   return minutesRemaining + ":" + secondsRemaining
 
 $(document).ready(() ->
+  # TODO: The logic in here is getting complicated. Time to
+  # refactor it
+  console.log("pomotodo ready")
+
   intervalID = null
 
   # 25 minutes
   remainingTime = 1000 * 60 * 1 
 
-  console.log("pomotodo ready")
-
   $('ul#todo_list li').click(show_pomodoro_timer)
 
+  $('h1#pomodoro_timer').text(format_countdown(remainingTime))
+
+  pomodoroTick = () ->
+    remainingTime = remainingTime - 1000
+    $('h1#pomodoro_timer').text(format_countdown(remainingTime))
+
+    if remainingTime <= 0
+      clearInterval(intervalID)
+      sound_alarm()
+
   $('button#pomodoro_start').click(() ->
-    tick = (t) ->
-      remainingTime = remainingTime - 1000
-      $('h1#display_timer').text(format_countdown(remainingTime))
-
-      if remainingTime <= 0
-        clearInterval(intervalID)
-        sound_alarm()
-
     console.log("you started a pomodoro")
-    intervalID = setInterval(tick, 1000)
+    intervalID = setInterval(pomodoroTick, 1000)
     toggle_buttons()
   )
+
+  # 15 seconds
+  interruptTime = 1000 * 15
 
   $('button#pomodoro_interrupt').click(() -> 
     console.log("you interrupted a pomodoro")
+
+    tick = () -> 
+      interruptTime = interruptTime - 1000
+      $('#interrupt_timer').text(format_countdown(interruptTime))
+
+      if interruptTime <= 0
+        clearInterval(intervalID)
+
     clearInterval(intervalID)
-    toggle_buttons()
+    intervalID = setInterval(tick, 1000)
+
+    $('#pomodoro_timer').toggle()
+    $('#interrupt_timer').toggle()
+
+    $('button#pomodoro_interrupt').attr('disabled', true)
+    $('#pomodoro_resume').attr('disabled', false)
+    $('button#pomodoro_start').attr('disabled', true)
   )
 
+  $('button#pomodoro_resume').click(() ->
+    console.log("resume the pomodoro")
+    clearInterval(intervalID)
+    intervalID = setInterval(pomodoroTick, 1000)
+
+    $('#pomodoro_timer').toggle()
+    $('#interrupt_timer').toggle()
+
+    $('button#pomodoro_interrupt').attr('disabled', false)
+    $('button#pomodoro_resume').attr('disabled', true)
+    $('button#pomodoro_start').attr('disabled', true)
+    interruptTime = 1000 * 15
+    $('#interrupt_timer').text(format_countdown(interruptTime))
+  )
+
+
   $('button#test_sound').click(() ->
-    console.log("play a sound!")
     sound_alarm()
   )
 )
--- src/short-samples/pomodoro/public/javascripts/client.js
+++ src/short-samples/pomodoro/public/javascripts/client.js
@@ -1,9 +1,9 @@
 (function() {
-  var format_countdown, show_pomodoro_timer, sound_alarm, toggle_buttons;
+  var format_countdown, pomodoro_object, show_pomodoro_timer, sound_alarm, toggle_buttons;
   show_pomodoro_timer = function(e) {
     console.log($(this).attr('id'));
     $('#todo_form').hide();
-    return $('#pomodoro_timer').show();
+    return $('#pomodoro').show();
   };
   toggle_buttons = function() {
     var a, b;
@@ -14,6 +14,12 @@
   };
   sound_alarm = function() {
     return $('#audiofile')[0].play();
+  };
+  pomodoro_object = {
+    InitialRemainingTime: 1000 * 60 * 25,
+    InitialInterruptTime: 1000 * 15,
+    remainingTime: -1,
+    interruptTime: -1
   };
   format_countdown = function(milliseconds) {
     var minutesRemaining, secondsRemaining;
@@ -29,32 +35,57 @@
     return minutesRemaining + ":" + secondsRemaining;
   };
   $(document).ready(function() {
-    var intervalID, remainingTime;
+    var interruptTime, intervalID, pomodoroTick, remainingTime;
+    console.log("pomotodo ready");
     intervalID = null;
     remainingTime = 1000 * 60 * 1;
-    console.log("pomotodo ready");
     $('ul#todo_list li').click(show_pomodoro_timer);
+    $('h1#pomodoro_timer').text(format_countdown(remainingTime));
+    pomodoroTick = function() {
+      remainingTime = remainingTime - 1000;
+      $('h1#pomodoro_timer').text(format_countdown(remainingTime));
+      if (remainingTime <= 0) {
+        clearInterval(intervalID);
+        return sound_alarm();
+      }
+    };
     $('button#pomodoro_start').click(function() {
+      console.log("you started a pomodoro");
+      intervalID = setInterval(pomodoroTick, 1000);
+      return toggle_buttons();
+    });
+    interruptTime = 1000 * 15;
+    $('button#pomodoro_interrupt').click(function() {
       var tick;
-      tick = function(t) {
-        remainingTime = remainingTime - 1000;
-        $('h1#display_timer').text(format_countdown(remainingTime));
-        if (remainingTime <= 0) {
-          clearInterval(intervalID);
-          return sound_alarm();
+      console.log("you interrupted a pomodoro");
+      tick = function() {
+        interruptTime = interruptTime - 1000;
+        $('#interrupt_timer').text(format_countdown(interruptTime));
+        if (interruptTime <= 0) {
+          return clearInterval(intervalID);
         }
       };
-      console.log("you started a pomodoro");
-      intervalID = setInterval(tick, 1000);
-      return toggle_buttons();
-    });
-    $('button#pomodoro_interrupt').click(function() {
-      console.log("you interrupted a pomodoro");
       clearInterval(intervalID);
-      return toggle_buttons();
+      intervalID = setInterval(tick, 1000);
+      $('#pomodoro_timer').toggle();
+      $('#interrupt_timer').toggle();
+      $('button#pomodoro_interrupt').attr('disabled', true);
+      $('#pomodoro_resume').attr('disabled', false);
+      return $('button#pomodoro_start').attr('disabled', true);
+    });
+    $('button#pomodoro_resume').click(function() {
+      console.log("resume the pomodoro");
+      clearInterval(intervalID);
+      intervalID = setInterval(pomodoroTick, 1000);
+      $('#pomodoro_timer').toggle();
+      $('#interrupt_timer').toggle();
+      $('button#pomodoro_interrupt').attr('disabled', false);
+      $('button#pomodoro_resume').attr('disabled', true);
+      $('button#pomodoro_start').attr('disabled', true);
+      interruptTime = 1000 * 15;
+      return $('#interrupt_timer').text(format_countdown(interruptTime));
     });
     return $('button#test_sound').click(function() {
-      console.log("play a sound!");
       return sound_alarm();
     });
   });
